bb8c51e77f900393d8bc06cce051ed203b2a910f
Luis Gonzalez <LGonzalez@garden.local>
2018-01-26 17:52:34 -0300

    manejo de progress

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { PagesComponent } from './pages.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
@@ -11,6 +12,8 @@ import { SharedModule } from './../shared/shared.module';
 
 //rutas relacionadas a las paginas
 import { PAGES_ROUTES } from './pages.routes';
+
+
 
 @NgModule({
     declarations: [
@@ -27,7 +30,8 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
   })
   export class PagesModule { }
